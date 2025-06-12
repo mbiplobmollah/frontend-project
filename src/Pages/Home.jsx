67dc7f9382a911357ac461b088580ac78090c968
mbiplobmollah <biplobmollah16@gmail.com>
2025-06-12 23:33:09 +0600

    fix an issue

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,8 +4,8 @@ function Home(){
 
     const movies = [
         {id:1 , title: 'John Wick', release_date:'2020'},
-        {id:1 , title: 'Terminator', release_date:'1993'},
-        {id:1 , title: 'The Matrix', release_date:'1995'}
+        {id:2 , title: 'Terminator', release_date:'1993'},
+        {id:3 , title: 'The Matrix', release_date:'1995'}
     ]
 
     const handleSearch = () =>{
