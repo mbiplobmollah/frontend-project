1fc99fef873d1ac2edcae1fc271fdeb101a620a0
mbiplobmollah <biplobmollah16@gmail.com>
2025-06-14 23:24:46 +0600

    work with state and search"

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,9 @@
 import MovieCard from "../components/MovieCard/MovieCard"
+import { useState } from "react"
 
 function Home(){
+
+    const [searchQuery, setSearchQuery] = useState('')
 
     const movies = [
         {id:1 , title: 'John Wick', release_date:'2020'},
@@ -9,16 +12,16 @@ function Home(){
     ]
 
     const handleSearch = () =>{
-
+        alert(searchQuery)
     }
 
     return(
         <div className="home">
             <h1>This is movie project</h1>
           <form onSubmit={handleSearch} className="search-form">
-            <input type="text" placeholder="Search For Movies..." className="search-input"/>
-            </form> 
+            <input type="text" placeholder="Search For Movies..." className="search-input" value={searchQuery} onChange={(e)=>{setSearchQuery(e.target.value)}}/>
             <button type="submit" className="search-button">Search</button> 
+            </form> 
         <div className="movies-grid">
             {
             movies.map(movie=><MovieCard movie={movie} key={movie.id}></MovieCard>)
